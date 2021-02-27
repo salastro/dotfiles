884a8c0db7af2245957bee012092bb8b25268535
salahdin-ahmed <salah2112004@gmail.com>
2021-02-27 20:21:56 +0200

    Add pywal slock template

---
+++ .config/wal/templates/colors-wal-slock.h
@@ -0,0 +1,5 @@
+static const char *colorname[NUMCOLS] = {{
+	[INIT] =   "{color0}",     /* after initialization */
+	[INPUT] =  "{color4}",   /* during input */
+	[FAILED] = "{color1}",   /* wrong password */
+}};
